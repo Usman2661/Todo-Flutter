4d534b376c06623869ccd474e1e5ac9c1213a305
Usman Ali <usmanusman136@hotmail.com>
2020-09-10 17:36:46 +0100

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:todo/screens/home.dart';
+import 'package:todo/screens/loading.dart';
+
+void main(){
+     runApp(MaterialApp(
+    initialRoute: '/home',
+    routes: {
+      '/': (context) => Loading(),
+      '/home': (context) => Home(),
+    },
+  ));
+}
---
+++ lib/screens/loading.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class Loading extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.teal[400],
+      body: Text('This is Loading Screen'),
+    );
+  }
+}
